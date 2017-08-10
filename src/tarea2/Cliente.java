2493285f3a65140087b57dad04e072d215c943cf
Andres <Andres@Andres-PC>
2017-08-09 21:00:38 -0500

    Implementacion de Hospital y CentroComercial

--- src/tarea2/Cliente.java
+++ src/tarea2/Cliente.java
@@ -6,8 +6,16 @@ public class Cliente {
         
         Constructora ConsCR = new ConstructoraCR("Amarilo");
         Edificio CR = ConsCR.construir("plaza central", "madera", 500, 4);
+        Constructora ConsCC = new ConstructoraCC("Capital");
+        Edificio CC=ConsCC.construir("Hayuelos", "Marmol", 1500, 3);
+        Constructora ConsH = new ConstructoraH("San Rafael");
+        Edificio H = ConsH.construir("San Rafael", "Concreto", 1000, 5);
         System.out.println(CR.autoLimpieza());
         System.out.println(CR.prenderLuces());
+        System.out.println(CC.autoLimpieza());
+        System.out.println(CC.prenderLuces());
+        System.out.println(H.autoLimpieza());
+        System.out.println(H.prenderLuces());
         
     }
     
